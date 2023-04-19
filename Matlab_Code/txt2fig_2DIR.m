e5d776822225b6ac2d90f2cd7dd55006b24ec0ea
dkurodalab <130791140+dkurodalab@users.noreply.github.com>
2023-04-19 11:44:51 -0500

    Update txt2fig_2DIR.m

--- Matlab_Code/txt2fig_2DIR.m
+++ Matlab_Code/txt2fig_2DIR.m
@@ -3,7 +3,7 @@ function txt2fig_2DIR(fig_folder)
     % Convert 2DIR .txt file into matlab figures at each waiting time
     % Save the generated matlab figures in a new folder (fig_folder) in the same directory as .txt file
 %%
-cf=pwd;
+cfi=pwd;
 cd(uigetdir)
 wtau=readmatrix('wtau_freq.txt');
 wt=readmatrix('wt_freq.txt');
@@ -19,5 +19,6 @@ sp=mkdir(fig_folder);
         savefig([erase(txt_list(n).name,'.txt'),'.fig'])
         cd(cf);
     end
-    cd(cf);
+    close all
+    cd(cfi);
 end
